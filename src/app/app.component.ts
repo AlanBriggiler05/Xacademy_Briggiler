b83c5b09c528df897a5387c16b9aac6fead3bba8
AlanBriggiler05 <a.briggiler.emser@gmail.com>
2023-07-11 17:40:12 -0300

    Btn y lista  de productos y costos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  nombre: string;
+  costo: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Xacademy_Briggiler';
+  items: Item[] = [
+    { nombre: 'Producto 1', costo: '$10' },
+    { nombre: 'Producto 2', costo: '$20' },
+    { nombre: 'Producto 3', costo: '$30' }
+  ];
+  mostrarLista = true;
+
+  toggleList() {
+    this.mostrarLista = !this.mostrarLista;
+  }
 }
